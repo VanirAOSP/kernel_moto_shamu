e797727cb8e14b6b9c8de9d69e34d128ccd361d9
nuclearmistake <nuclearmistake@gmail.com>
2015-07-29 08:08:27 -0400

    Fix undefined behavior in cpufreq_interactive: ret assigned twice in same statement
    Change-Id: I8c4196b4eafb686c406cb9c10d7850a82faf957d

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -783,8 +783,7 @@ static ssize_t show_target_loads(
 	for (i = 0; i < ntarget_loads; i++)
 		ret += sprintf(buf + ret, "%u%s", target_loads[i],
 			       i & 0x1 ? ":" : " ");
-
-	ret += sprintf(buf + --ret, "\n");
+	ret += sprintf(buf + ret - 1, "\n");
 	spin_unlock_irqrestore(&target_loads_lock, flags);
 	return ret;
 }
@@ -827,7 +826,7 @@ static ssize_t show_above_hispeed_delay(
 		ret += sprintf(buf + ret, "%u%s", above_hispeed_delay[i],
 			       i & 0x1 ? ":" : " ");
 
-	ret += sprintf(buf + --ret, "\n");
+	ret += sprintf(buf + ret - 1, "\n");
 	spin_unlock_irqrestore(&above_hispeed_delay_lock, flags);
 	return ret;
 }
